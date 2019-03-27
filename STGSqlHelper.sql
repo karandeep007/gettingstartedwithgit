b2e33fe98892302e9e72519cb9c00cfc2c28b72a
Karandeep Singh <karan.redrock@gmail.com>
2019-03-27 12:29:31 +0530

    Adding Vipul's DDL
    -RX30DDLS

---
+++ STGSqlHelper.sql
@@ -0,0 +1,8 @@
+#This is a Test file to start work on Git repo form second account
+
+select * from config.newSoures;
+
+update config.newSources set WaterMarkValue = '2018-01-01';
+
+
+
---
+++ table ddls.sql
@@ -0,0 +1,17 @@
+create table doctor(
+name varchar(20),
+id varchar(30),
+age int,
+salary float
+);
+
+create table patient(
+name varchar(30),
+insurance_type varchar(40),
+dateoflastvisit datetime2
+);
+
+create table drug(
+name varchar(50),
+WAP int,
+type varchar(50))
